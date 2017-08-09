4d0b845ffa3dcace0a998713a1d279d28e1747ac
kornelM <makusz07@gmail.com>
2017-08-09 13:44:11 +0200

    Added new input two-way binded

--- src/app/dev/app.component.ts
+++ src/app/dev/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   template: `
     {{onTest()}}
-    <input type="text" [value]="name" [ngClass] ="'red'"  (keyup)="onKeyup(inputElement.value)"  #inputElement> <br>
+    <input type="text" [value]="name" [ngClass] ="'red'"  (keyup)="onKeyup(inputElement.value)"  #inputElement/> 
+    <br>
     <p>{{values}}</p>
+    <br>
+    <br>
+    <input text="text" [(ngModel)]="name"/>
+    <p>Your name: {{name}}</p>
   `,
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  name = 'Kornel';
+  name: String = '';
   values = '';
 
   onTest() {
